eac0ae555a82fc75f775a6f414f0ad89bde1f89b
yzx <569390341@qq.com>
2015-04-30 09:25:45 +0800

    no message

---
+++ MyMusic/MyMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMusic
+//
+//  Created by 展祥叶 on 15/4/30.
+//  Copyright (c) 2015年 yzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyMusic/MyMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMusic
+//
+//  Created by 展祥叶 on 15/4/30.
+//  Copyright (c) 2015年 yzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
